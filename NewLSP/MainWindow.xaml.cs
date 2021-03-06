15fdb28edca569380eaad540a47966b658f680cf
William Yarger <william.yarger.md@hotmail.com>
2021-03-06 15:52:30 -0500

    Add project files.

---
+++ NewLSP/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NewLSP.UserControls;
+
+namespace NewLSP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void miHome_Click(object sender, RoutedEventArgs e)
+        {
+            SetActiveUserControl(ucHome);
+        }
+
+        private void miSubjectTree_Click(object sender, RoutedEventArgs e)
+        {
+            SetActiveUserControl(ucSubjectTree);
+        }
+
+        private void miCreateQA_Click(object sender, RoutedEventArgs e)
+        {
+            SetActiveUserControl(ucCreatEditQA);
+        }
+
+        private void miTest_Click(object sender, RoutedEventArgs e)
+        {
+            SetActiveUserControl(ucTestReview);
+        }
+
+        public void SetActiveUserControl(UserControl control)
+        {
+            // Make the visibility of all user controls collapsed
+            ucHome.Visibility = Visibility.Collapsed;
+            ucSubjectTree.Visibility = Visibility.Collapsed;
+            ucCreatEditQA.Visibility = Visibility.Collapsed;
+            ucTestReview.Visibility = Visibility.Collapsed;
+
+
+           // Make the selected control visible
+           control.Visibility = Visibility.Visible;
+        }
+
+    }
+}
